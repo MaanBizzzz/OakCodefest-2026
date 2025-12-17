77410b612a5363fa3fe7f228e5963dbd476e69dc
LearnPRG-py <aryankrishnan4b@gmail.com>
2025-12-17 18:22:50 +0530

    Add image carosel and reviews section on oakmun page

--- AboutUs/script.js
+++ AboutUs/script.js
@@ -32,4 +32,40 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     type();
+    const scrollSpeed = 1;
+    let scrollInterval;
+
+    function setupCarousels(containerSelector, direction = 1) {
+        const containers = document.querySelectorAll(containerSelector);
+        return Array.from(containers).map(container => {
+        const carousel = container.querySelector('.carousel');
+        carousel.innerHTML += carousel.innerHTML;
+        const halfWidth = carousel.scrollWidth / 2;
+        if (direction === -1) {
+            container.scrollLeft = halfWidth;
+        }
+        return { container, halfWidth, direction };
+        });
+    }
+
+    function startAutoScroll() {
+        const rightCarousels = setupCarousels('.lazy-scrolling-container.right', 1);
+        const leftCarousels  = setupCarousels('.lazy-scrolling-container.left', -1);
+
+        scrollInterval = setInterval(() => {
+        rightCarousels.forEach(c => {
+            c.container.scrollLeft += scrollSpeed;
+            if (c.container.scrollLeft >= c.halfWidth) {
+            c.container.scrollLeft = 0;
+            }
+        });
+        leftCarousels.forEach(c => {
+            c.container.scrollLeft -= scrollSpeed;
+            if (c.container.scrollLeft <= 0) {
+            c.container.scrollLeft = c.halfWidth;
+            }
+        });
+        }, 16);
+    }
+    startAutoScroll();
 });
